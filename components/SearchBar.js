238b4b5307675f6e3f9d349665334879db8c6346
Farhod <farhod@MacBook-Air-Farhod.local>
2023-04-24 22:21:26 +0600

    hadlechange function refactor

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -6,6 +6,73 @@ import SearchModal from "@/components/SearchModal";
 import SearchDropDownPortal from "@/components/SearchDropDownPortal";
 
 
+const SearchBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const {games, setGames, searchGames} = useSearchGames();
+    const searchRef = React.useRef();
+
+
+    const handleClose = () => {
+        setIsOpen(false);
+    }
+
+    const handleClearInput = () => {
+        searchRef.current.value = "";
+        setGames([]);
+    }
+
+    const handleChangeInput = React.useCallback(async (event) => {
+        const query = event.target.value;
+
+        if (!query) {
+            setGames([]);
+            return;
+        }
+
+        try {
+            const games = await searchGames(query);
+            if (event.target.value === query) {
+                setGames(games.results);
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }, [searchGames]);
+
+    const lengthCheck = games && games.length > 0;
+
+    return (
+        <SearchContainer style={{marginLeft: '40px'}}>
+            <StyledInput
+                type='text'
+                onChange={handleChangeInput}
+                ref={searchRef}
+                placeholder='Название игры'
+                onFocus={() => setIsOpen(true)}
+            />
+            {lengthCheck && (
+                <ClearButton onClick={handleClearInput}>&#xd7;</ClearButton>
+            )}
+            {lengthCheck && (
+                <SearchDropDown
+                    onClose={handleClose}
+                    onClearInput={handleClearInput}
+                    games={games}
+                />
+            )}
+            {lengthCheck && (
+                <SearchModal>
+                    <SearchDropDownPortal onClearInput={handleClearInput} games={games}/>
+                </SearchModal>
+            )}
+        </SearchContainer>
+
+    );
+};
+
+export default SearchBar;
+
 const SearchContainer = styled.div`
   position: relative;
   width: 80%;
@@ -37,65 +104,3 @@ const ClearButton = styled.button`
   font-size: 20px;
   cursor: pointer;
 `;
-
-const SearchBar = () => {
-    const [value, setValue] = useState('');
-    const [isOpen, setIsOpen] = useState(false);
-    const {games, setGames, searchGames} = useSearchGames();
-
-    const lengthCheck = games && games.length > 0;
-
-    const handleClose = () => {
-        setIsOpen(false);
-    }
-
-    const handleClearInput = () => {
-        setValue('')
-        setGames([]);
-    }
-
-    const handleChangeInput = React.useCallback(async (event) => {
-        setValue(event.target.value);
-        const query = event.target.value;
-
-        try {
-            const games = await searchGames(query);
-            if (event.target.value === query) {
-                setGames(games.results);
-            }
-
-        } catch (error) {
-            console.error(error);
-        }
-    }, [searchGames]);
-
-    return (
-        <SearchContainer style={{marginLeft: '40px'}}>
-            <StyledInput
-                type='text'
-                onChange={handleChangeInput}
-                value={value}
-                placeholder='Название игры'
-                onFocus={() => setIsOpen(true)}
-            />
-            {lengthCheck && (
-                <ClearButton onClick={handleClearInput}>&#xd7;</ClearButton>
-            )}
-            {lengthCheck && (
-                <SearchDropDown
-                    onClose={handleClose}
-                    onClearInput={handleClearInput}
-                    games={games}
-                />
-            )}
-            {lengthCheck && (
-                <SearchModal>
-                    <SearchDropDownPortal onClearInput={handleClearInput} games={games}/>
-                </SearchModal>
-            )}
-        </SearchContainer>
-
-    );
-};
-
-export default SearchBar;
